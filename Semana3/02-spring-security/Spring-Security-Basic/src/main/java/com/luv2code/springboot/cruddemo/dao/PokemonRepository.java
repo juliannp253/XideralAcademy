04f27dc2aacc6ef1a65c4ec45ab5e219d0fa6a1b
Julian <julian.padronnunez03@gmail.com>
2026-08-28 16:21:04 -0500

    Proyecto con Spring Security Basic

---
+++ Semana3/02-spring-security/Spring-Security-Basic/src/main/java/com/luv2code/springboot/cruddemo/dao/PokemonRepository.java
@@ -0,0 +1,7 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Pokemon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PokemonRepository extends JpaRepository<Pokemon, Integer> {
+}
